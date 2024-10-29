48f932cb293c046c288abb0ad180f1fd3d851e90
Cathrine Nesterova <nev.hrd@gmail.com>
2024-10-29 22:43:20 +0300

    Добавлена функция сохранения в формате json

--- main.py
+++ main.py
@@ -4,6 +4,22 @@ from tkinter import messagebox as mb
 from tkinter import ttk
 import requests
 import pyperclip  # Библиотека для автоотправления ссылок в буфер обмена
+import json
+import os
+
+history_file = 'upload_history.json'
+
+
+def save_history(file_path, link):
+    history = []
+    if os.path.exists(history_file):
+        with open(history_file, "r") as f:
+            history = json.load(f)
+
+    history.append({"file_path": os.path.basename(file_path), "download_link": link})
+
+    with open(history_file, "w") as f:
+        json.dump(history, f, indent=4)
 
 
 def upload():
@@ -18,6 +34,7 @@ def upload():
                 entry.delete(0, END)  # Очищаем поле перед загрузкой нового файла
                 entry.insert(0, link)
                 pyperclip.copy(link)
+                save_history(filepath, link)
                 mb.showinfo('Ссылка скопирована', f'Ссылка {link} успешно скопирована в буфер обмена')
     except ValueError as ve:
         mb.showerror("Ошибка", f"Произошла ошибка: {ve}")
